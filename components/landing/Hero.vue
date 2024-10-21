b42e90912c2c1badca1036f33909c817d8e595e5
bilalkolli <bilal.kokoko@outlook.fr>
2024-10-21 13:24:58 +0200

    modification du copywriting et de la structuration du composant hero via une suppression de l'image du hero et une modification du css

--- components/landing/Hero.vue
+++ components/landing/Hero.vue
@@ -1,41 +1,28 @@
 <template>
-  <main class="grid lg:grid-cols-2 place-items-center pt-16 pb-8 md:pt-8">
-    <div class="p-24 md:order-1 hidden md:block">
-      <img
-        class="rounded-full transition-transform transform hover:scale-125"
-        src="~/assets/img/hero.jpg"
-        alt="Starship starts the engine"
-        loading="eager"
-        format="avif"
-        width="512"
-        height="512"
-      />
-    </div>
-
-    <div>
-      <h1 class="text-5xl lg:text-6xl xl:text-7xl font-bold lg:tracking-tight">
-        Marketing website done with Nuxt
+  <main class="pb-64 text-center md:pt-32">
+    <div class="py-20 sm:py-12 md:py-12 !pb-0">
+      <h1 class="text-5xl font-bold tracking-tight text-gray-900 sm:text-7xl">
+        Skyrock your knowledge with Nuxt
       </h1>
-      <p class="text-lg mt-4 text-slate-600 max-w-xl">
-        Nuxtship is a starter template for startups, marketing websites &
-        landing pages.<wbr /> Built with Nuxt & TailwindCSS. You can quickly
-        create any website with this starter.
+      <p class="text-lg mt-6 text-slate-600">
+        QuizNuxt is your go-to platform for interactive general knowledge quizzes. Built with Nuxt and TailwindCSS
       </p>
-      <div class="mt-6 flex flex-col sm:flex-row gap-3">
+      <div class="mt-6 flex flex-col sm:flex-row gap-3 justify-center">
         <LandingLink
           href="https://github.com/Gr33nW33n/nuxtship-template"
           target="_blank"
           rel="noopener"
-          >Get Started</LandingLink
         >
+        Get Started
+        </LandingLink>
         <LandingLink
           size="lg"
           styleName="outline"
           rel="noopener"
           href="https://github.com/Gr33nW33n/nuxtship-template"
           target="_blank"
-          >View Repo</LandingLink
         >
+          Create Quiz</LandingLink>
       </div>
     </div>
   </main>
