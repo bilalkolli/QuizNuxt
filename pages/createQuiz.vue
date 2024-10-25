2e776dbcfe9eb282cff3ab14f2535580f6acb9b0
bilalkolli <bilal.kokoko@outlook.fr>
2024-10-25 16:45:03 +0200

    Ajout de la structure de la page de création de quiz
    - Mise en place du layout 'landing'
    - Intégration du composant LandingContactform
    - Ajout d'un titre et d'une description pour encourager les utilisateurs à créer leur propre quiz

--- pages/createQuiz.vue
+++ pages/createQuiz.vue
@@ -1,6 +1,20 @@
-<template>
-
-</template>
-
 <script setup>
+definePageMeta({
+  layout: "landing"
+})
 </script>
+
+<template>
+  <LandingContainer>
+    <LandingSectionhead>
+      <template v-slot:title>Create your own Quiz</template>
+      <template v-slot:desc>Help people to improve theyre general knowledge.</template>
+    </LandingSectionhead>
+
+    <div class="mx-auto max-w-4xl mt-16">
+      <div>
+        <LandingContactform />
+      </div>
+    </div>
+  </LandingContainer>
+</template>
